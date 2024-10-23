750c25097f47a26ae87d045b35bf564aa4d167ea
Micaela Lirussi <micaelalirussi@hotmail.com>
2024-10-23 01:57:53 -0300

    borrar archivos innecesarios

--- Punto4Caso1.py
+++ Punto4Caso1.py
@@ -1,6 +1,5 @@
 #Sitio: Poke Api (https://pokeapi.co/api/v2)
-#Ejercicio 4
-#CASO 1
+#CASO1
 #Hacer un get a berry/1
 #Verificar que el size sea 20
 #Verificar que el soil_dryness sea 15
@@ -9,25 +8,31 @@
 import requests
 
 def test_berry_endpoint():
+    test_status = True
     url = "https://pokeapi.co/api/v2/berry/1"
 
     response = requests.get(url)
     print("Comenzando prueba")
     
     assert response.status_code == 200, f"Error: {response.status_code}. No se pudo acceder al endpoint."
-    print("Codigo 200 recibido de la api")
+    print("Respuesta positiva de la API")
 
     berry_data = response.json()
+    
+    print(f"El tamaño de la berry es: {berry_data['size']}")
+    if berry_data['size'] != 20: test_status = False
+    print(True if berry_data['size'] == 20 else False)
 
-    assert berry_data['size'] == 20, f"El tamaño esperado es 20, pero se recibió {berry_data['size']}."
-    print("El tamaño de berry es 20")
+    print(f"La sequedad del suelo esperada es: {berry_data['soil_dryness']}")
+    if berry_data['soil_dryness'] != 15: test_status = False
+    print(True if berry_data['soil_dryness'] == 15 else False)
 
-    assert berry_data['soil_dryness'] == 15, f"La sequedad del suelo esperada es 15, pero se recibió {berry_data['soil_dryness']}."
-    print("El 'soil_dryness de berry es 15")
+    print(f"La firmeza del suelo es: {berry_data['firmness']['name']}")
+    if berry_data['firmness']['name'] != "soft": test_status = False
+    print(True if berry_data['firmness']['name'] == "soft" else False)
 
-    assert berry_data['firmness']['name'] == "soft", f"El nombre de firmeza esperado es 'soft', pero se recibió '{berry_data['firmness']['name']}'."
-    print("El firmness_name es 'soft'")
-    print("Prueba finalizada")
+    if test_status: print("Pokedex actualizada con exito")
+    else: print("Prueba finalizada sin exito")
 
 if __name__ == "__main__":
     test_berry_endpoint()
